6a4690fe681b4b49c01e88c017164a6cc3886056
MaterialesSecundaria <79664447+MaterialesSecundaria@users.noreply.github.com>
2024-01-08 13:00:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,707 @@
+  /* DESCARGA PPT 2023*/
+  const featuresList = [
+    {
+      icon: "assets/icono_par.png",
+      title: "Álgebra ",
+      description:
+        "PPT 2023.",
+      link: "https://drive.google.com/drive/folders/1NHsQlLgvYq8Q8tBtZvPJ20qt_fKl2cJW?usp=drive_link",
+    },
+  
+    {
+      icon: "assets/icono_arit.png",
+      title: "Aritmética",
+      description:
+        "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1Xsr0qFC3OydrD-BLIh6VWzHUd195tER0?usp=drive_link",
+    },
+  
+    {
+      icon: "assets/icono_geoms.png",
+      title: "Geometría",
+      description:
+        "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1DpeaddLPxhZUwEtIrLEN9iyUpK5YjTzh?usp=drive_link",
+    },
+  
+    {
+      icon: "assets/icono_trigs.png",
+      title: "Trigonometría",
+      description:
+        "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/11MejwXRoPJGstDE7N59D_2-hluKBVD9Y?usp=drive_link",  
+    },
+  
+    {
+      icon: "assets/icono_comp.png",
+      title: "Raz. Matemático",
+      description:
+        "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1B8S_wO4XKSmLB-KmnFhNmgTqExwBtdTM?usp=drive_link",   
+    },
+  
+    {
+      icon: "assets/icono_fis.png",
+      title: "Física",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1r5OZ8ZC3G-uzNhiTPaIS5KkkmbEmIvCr?usp=drive_link",
+    },
+     {
+      icon: "assets/icono_quim.png",
+      title: "Química",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1kVlF_Qt6ZpIMBPy2ZDnqSftf-Id7EvCr?usp=drive_link",
+    },
+    {
+      icon: "assets/biology.png",
+      title: "Biología",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1FuOIthXjFqLPj80z_SsoBWglBos6dev5?usp=drive_link",
+    },
+     {
+      icon: "assets/icono_leng.png",
+      title: "Lenguaje",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1Oi54oFLrACrmNrhb0sx4x98XHJyiC0a0?usp=drive_link",
+    },
+    {
+      icon: "https://res.cloudinary.com/dr8dnxnca/image/upload/v1689395117/blogger_1/create-task-icon_bjbqxc.svg",
+      title: "Literatura",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1Z_jv3hT8OQNn59fvw4tMzMkan2lhhP2e?usp=drive_link",
+    },
+      {
+      icon: "assets/icono_rv.png",
+      title: "Raz Verbal",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1d9mybVWmANDn-PVRdQIhwGEAJv9321-C?usp=drive_link",
+    },
+    {
+      icon: "assets/ingles.png",
+      title: "Inglés",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1pG6RDcAX4irqgwJJlEYbQ9NHxOFJCi7J?usp=drive_link",
+    },
+    {
+      icon: "assets/tutoria.png",
+      title: "Tutoria",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1J9-9Svl1suDos4J1lfQeqLfVMI4u-V2w?usp=drive_link",
+    },
+    {
+      icon: "assets/icons8-theatre-mask-64.png",
+      title: "Teatro",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1JmuQAta_hwvD5O6JbjAoz7mDXLGMUS6O?usp=drive_link",
+    },
+    {
+      icon: "assets/peruvian.png",
+      title: "H. Perú",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1NXyiJD3-qPsOubHQ8xjkbJqWfWqh7KiV?usp=drive_link",
+    },
+    {
+      icon: "assets/geografy.png",
+      title: "Geografía",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1nPoaNVbXRxBeHi2ES1buf7n7P7Kqns8Z?usp=drive_link",
+    },
+    {
+      icon: "assets/icono_geom.png",
+      title: "H. Universal",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1YvcgtUoOL6zG6BAldbe5DdV033lMnY_Q?usp=drive_link",
+    },
+    {
+      icon: "assets/icono-economy-80.png",
+      title: "Economía",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1tAhcpHNTlLPuVmd-scVHD0wYU_uUq_0c?usp=drive_link",
+    },
+    {
+      icon: "assets/psicology.png",
+      title: "Psicología",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1Epw4u7hXk8UxwaperilaNybo1SECGh3s?usp=drive_link",
+    },
+    {
+      icon: "assets/valores.png",
+      title: "Valores",
+      description:
+      "PPT 2023.",
+      link:"https://drive.google.com/drive/folders/1_vskq-Qb7b_Db84nm_ve92P6T5ppfOX-?usp=drive_link",
+    },
+  
+      
+  ];
+    /* CARGA PPT 2024*/
+  const featuresListDos = [
+    {
+      icon: "assets/icono_par.png",
+      title: "Álgebra ",
+      description:
+        "PPT 2024.",
+      link: "https://drive.google.com/drive/folders/1xfU9Dg9ZJEvMN26tYDIxSOCjnVyBaVMe?usp=sharing",
+    },
+  
+    {
+      icon: "assets/icono_arit.png",
+      title: "Aritmética",
+      description:
+        "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1DHIESsAWonublEogupU0TiJMfIiBrHuw?usp=sharing",
+    },
+  
+    {
+      icon: "assets/icono_geoms.png",
+      title: "Geometría",
+      description:
+        "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1B_ouMrlpnXw2IGL2AsCVrFlUQ7ohSZwT?usp=sharing",
+    },
+  
+    {
+      icon: "assets/icono_trigs.png",
+      title: "Trigonometría",
+      description:
+        "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1v7vHNVyrgsitQ9XyjDllnoJzd5oKtNva?usp=sharing",  
+    },
+  
+    {
+      icon: "assets/icono_comp.png",
+      title: "Raz. Matemático",
+      description:
+        "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1UMj4mWR7SBvp2k2ALEZFDeW4yQvba0_V?usp=sharing",   
+    },
+  
+    {
+      icon: "assets/icono_fis.png",
+      title: "Física",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1eZCgnB4pvFpOdcFZjvgamekTlNXYKXm3?usp=sharing",
+    },
+     {
+      icon: "assets/icono_quim.png",
+      title: "Química",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1uZd9v3JsJF_8xIGlV-E6jceAi4brmthz?usp=sharing",
+    },
+    {
+      icon: "assets/biology.png",
+      title: "Biología",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/1uZd9v3JsJF_8xIGlV-E6jceAi4brmthz?usp=sharing",
+    },
+     {
+      icon: "assets/icono_leng.png",
+      title: "Lenguaje",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/14Ej7k8GPvptXB6_-2fz7G9EzBkBsp3Jt?usp=sharing",
+    },
+    {
+      icon: "https://res.cloudinary.com/dr8dnxnca/image/upload/v1689395117/blogger_1/create-task-icon_bjbqxc.svg",
+      title: "Literatura",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/14Ej7k8GPvptXB6_-2fz7G9EzBkBsp3Jt?usp=sharing",
+    },
+      {
+      icon: "assets/icono_rv.png",
+      title: "Raz Verbal",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    {
+      icon: "assets/ingles.png",
+      title: "Inglés",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    {
+      icon: "assets/tutoria.png",
+      title: "Tutoria",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    {
+      icon: "assets/icons8-theatre-mask-64.png",
+      title: "Teatro",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    {
+      icon: "assets/peruvian.png",
+      title: "H. Perú",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    {
+      icon: "assets/geografy.png",
+      title: "Geografía",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    {
+      icon: "assets/icono_geom.png",
+      title: "H. Universal",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    {
+      icon: "assets/icono-economy-80.png",
+      title: "Economía",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    {
+      icon: "assets/psicology.png",
+      title: "Psicología",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    {
+      icon: "assets/valores.png",
+      title: "Valores",
+      description:
+      "PPT 2024.",
+      link:"https://drive.google.com/drive/folders/15AwrliQQ8YwTDZNFPG_NfMh_nbSltVa9?usp=sharing",
+    },
+    
+  ];  
+  /* Compendios*/
+  const specificationsData = [
+    {
+      category: "Área Matemática",
+      details: [
+        {
+          title: "Álgebra",
+          value: "Descarga Compendio",
+          link:  "/.",
+        },
+        {
+          title: "Aritmética",
+          value: "Descarga Compendio",
+          link:  "/.",
+        },
+        {
+          title: "Geometría",
+          value: "Descarga Compendio",
+          link:  "/.",
+        },
+        {
+          title: "Trigonometría",
+          value: "Descarga Compendio",
+          link:  "/.",
+        },
+        {
+          title: "Raz. Matemático",
+          value: "Descarga Compendio",
+          link:  "/.",
+        },
+        
+        
+      ],
+    },
+  
+    {
+      category: "Área C.T.",
+      details: [
+        {
+          title: "Física",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Química",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Bíología",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        
+      ],
+    },
+    {
+      category: "Área Comunicación",
+      details: [
+        {
+          title: "Lenguaje",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Literatura",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Raz. Verbal",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Teatro",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        
+      ],
+    },
+    {
+      category: "Área CC.SS",
+      details: [
+        {
+          title: "H. Perú",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Geografía",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "H. Universal",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Economía",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Psicología",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Valores",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        
+        
+      ],
+    },
+  ];
+  /* BALOTARIO */
+  const specificationsDataTwo = [
+    {
+      category: "Matemática",
+      details: [
+        {
+          title: "Álgebra",
+          value: "Descarga Balotario",
+          link:  "./",
+        },
+        {
+          title: "Arimética",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Geometría",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Trigonometría",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+        {
+          title: "Raz. Matemático",
+          value: "Descarga Compendio",
+          link:  "./",
+        },
+              
+        
+      ],
+    },
+  
+    {
+      category: "Boletines Filter 2",
+      details: [
+        {
+          title: "Capítulo #1",
+          value: "Octubre 23, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #2",
+          value: "Octubre 23, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #3",
+          value: "Octubre 23, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #4",
+          value: "Noviembre 11, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #5",
+          value: "Noviembre 11, 2023 (filtro #2)",
+        },
+        {
+          title: "Capítulo #6",
+          value: "Noviembre 11, 2023 (filtro #2)",
+        },
+      ],
+    },
+    {
+      category: "PPT'S actualizaciones",
+      details: [
+        {
+          title: "Capítulo #1",
+          value: "Noviembre 03, 2023",
+        },
+        {
+          title: "Capítulo #2",
+          value: "Noviembre 10, 2023",
+        },
+        {
+          title: "Capítulo #3",
+          value: "Noviembre 17, 2023",
+        },
+        {
+          title: "Capítulo #4",
+          value: "Noviembre 24, 2023",
+        },
+        {
+          title: "Capítulo #5",
+          value: "Diciembre 01, 2023",
+        },
+        {
+          title: "Capítulo #6",
+          value: "Diciembre 08, 2023",
+        },
+      ],
+    },
+  ];
+
+  const specificationsContainer = document.querySelector(".specifications-container");
+  const specificationsContainerTwo = document.querySelector(".specifications-container-two");
+  const featuresContent = document.querySelector("#features .content");  
+  const featuresContentDos = document.querySelector("#ppt23 .content");  
+    
+  const menuIcon = document.querySelector(".menu-icon");
+  const mobileNavMenu = document.querySelector(".mobile-nav-menu");
+  const navLinks = document.querySelectorAll(".nav-link");
+  let currentTestimonialIndex = 0;  
+
+  const filterList = document.querySelector(".filter");
+  const filterButtons = filterList.querySelectorAll(".filter-btn");
+  const conferences = document.querySelectorAll(".conference");
+
+  let conferenceIndex = 0;
+    
+  /* ppt 2024-CARGA*/  
+  const displayFeatures = () => {
+    featuresList.forEach((f) => {
+      const html = `<div class="icon">
+          <img src="${f.icon}" alt="" />
+        </div>
+        <h3>${f.title}</h3>
+        <p>
+          ${f.description}
+        </p>
+         <a href="${f.link}" target='_blank' class="btn-dos">Ingrese</a>`;
+  
+      const featureCard = document.createElement("div");
+      featureCard.classList.add("feature-card");
+      featureCard.innerHTML = html;
+  
+      featuresContent.appendChild(featureCard);
+    });
+  };
+  
+  displayFeatures();
+  /*End  ppt 2024*/  
+    
+  /* boletin 2024-DESCARGA*/
+    
+    const displayFeaturesDos = () => {
+    featuresListDos.forEach((f) => {
+      const htmlune = `<div class="icon">
+          <img src="${f.icon}" alt="" />
+        </div>
+        <h3>${f.title}</h3>
+        <p>
+          ${f.description}
+        </p>
+         <a href="${f.link}" target='_blank' class="btn">Ingrese</a>`;
+  
+      const featureCardDos = document.createElement("div");
+      featureCardDos.classList.add("feature-card");
+      featureCardDos.innerHTML = htmlune;
+  
+      featuresContentDos.appendChild(featureCardDos);
+    });
+  };
+  
+  displayFeaturesDos();
+  
+  /* End boletin 2024-DESCARGA */
+    
+   
+  /* MENU-MOBIL */
+  menuIcon.addEventListener("click", () => {
+    mobileNavMenu.classList.toggle("active");
+  });
+  
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
+  
+      if (targetElement) {
+        const offset = targetElement.offsetTop - 60;
+        window.scrollTo({ top: offset });
+      }
+  
+      mobileNavMenu.classList.remove("active");
+    });
+  });  
+
+/* END-MENU-MOBIL */
+/* COMPENDIO */
+  const generateHTML = () => {
+    specificationsData.forEach((s) => {
+      const tableRowsHTML = s.details
+        .map((detail) => {
+          return `<tr>
+          <th>${detail.title}</th>
+          <td><a href="${detail.link}" target='_blank'>
+          ${detail.value}</a></td>
+          
+        </tr>`;
+        })
+        .join("");
+  
+      const HTML = `<h3>
+          ${s.category} (${s.details.length})
+          <span class="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </span>
+        </h3>
+        <table>
+            <tbody>
+              ${tableRowsHTML}
+            </tbody>
+          </table>`;
+  
+      const category = document.createElement("div");
+      category.classList.add("category");
+      category.innerHTML = HTML;
+  
+      specificationsContainer.appendChild(category);
+    });
+  };
+  
+  generateHTML();
+  
+  const categoryHeadings = document.querySelectorAll(
+    ".specifications-container h3"
+  );
+  
+  categoryHeadings.forEach((h) => {
+    h.addEventListener("click", () => {
+      h.classList.toggle("active");
+      const table = h.nextElementSibling;
+  
+      if (h.classList.contains("active")) {
+        table.style.display = "table";
+        return;
+      }
+  
+      table.style.display = "none";
+    });
+  });
+/* END-COMPENDIO */
+  
+conferences.forEach((conference) => {
+  conference.style.viewTransitionName = `conf-${++conferenceIndex}`;
+});
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let confCategory = e.target.getAttribute("data-filter");
+
+    if (!document.startViewTransition) {
+      updateActiveButton(e.target);
+      filterEvents(confCategory);
+    }
+
+    document.startViewTransition(() => {
+      updateActiveButton(e.target);
+      filterEvents(confCategory);
+    });
+  });
+});
+
+function updateActiveButton(newButton) {
+  filterList.querySelector(".actives").classList.remove("actives");
+  newButton.classList.add("actives");
+}
+
+function filterEvents(filter) {
+  conferences.forEach((conference) => {
+    // get each conferences category
+    let eventCategory = conference.getAttribute("data-category");
+
+    // check if that category matches with the filter
+    if (filter === "all" || filter === eventCategory) {
+      conference.removeAttribute("hidden");
+    } else {
+      conference.setAttribute("hidden", "");
+    }
+  });
+}
+ 
